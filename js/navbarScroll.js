1dc6dfbbff8fb4762897394d63be4b441f6d5c61
Fatima Bitar <fatimabitar97@gmail.com>
2021-12-05 12:21:49 +0200

    ....

--- js/navbarScroll.js
+++ js/navbarScroll.js
@@ -1,26 +1,12 @@
-//Select All links
-const allLinks = document.querySelectorAll(".navbar .navbar-nav .nav-item a");
 
-function scroolToSomeWhere(elements) {
-  elements.forEach((ele) => {
-    ele.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(e.target.dataset.section).scrollIntoView({
-        behavior: "smooth",
-      });
-    });
-  });
-}
-
-scroolToSomeWhere(allLinks);
 
 let navbar = document.querySelector(".navbar");
-
+console.log(navbar);
 window.onscroll = () => {
-  if (window.pageYOffset > 1000) {
-    navbar.classList.add("bg-bluee");
+  if (window.pageYOffset > 500) {
+    navbar.classList.add("bg-blue");
     
   } else {
-    navbar.classList.remove("bg-bluee");
+    navbar.classList.remove("bg-blue");
   }
 };
